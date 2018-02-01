e671173ff3488a68c0478e3d1641cd523007105c
Lipen <lipen00@gmail.com>
2018-02-01 16:34:44 +0300

    Add url, add numpy and scipy to deps, suggest colorama

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ setup(
     name='HMMLE',
     version='1.0.0',
     description='Hybridization Models Maximum Likelihood Estimator',
+    url='https://github.com/Lipen/hmmle',
     author='Konstantin Chukharev',
     author_email='lipen00@gmail.com',
-    py_modules=['hmmle', 'models'],
+    py_modules=['hmmle'],
     install_requires=[
+        'numpy',
+        'scipy',
         'Click',
     ],
+    extras_require={
+        'dev': ['colorama']
+    },
     entry_points={
         'console_scripts': [
             'hmmle = hmmle:cli',
