57e14db57a7f8445711e552bc83dcfa0db7d59dc
Lipen <lipen00@gmail.com>
2018-02-02 13:47:33 +0300

    Fix version handling

--- version.py
+++ version.py
@@ -2,38 +2,30 @@ __all__ = ('get_version', )
 
 import re
 import subprocess
-from os.path import dirname, isdir, join
 
 regex_version = re.compile(r'v?(.+)-(\d+)-g(\w+)(-dirty)?')
 
 
 def get_version():
-    d = dirname(__file__)
+    # Get the version using "git describe".
+    cmd = 'git describe --tags --long --dirty --always'
+    try:
+        git_version = subprocess.check_output(cmd, shell=True).decode().strip()
+    except subprocess.CalledProcessError:
+        print('Unable to get version number from git tags')
+        exit(1)
 
-    if isdir(join(d, '.git')):
-        # Get the version using "git describe".
-        cmd = 'git describe --tags --long --dirty --always'
-        try:
-            version = subprocess.check_output(cmd, shell=True).decode().strip()
-        except subprocess.CalledProcessError:
-            print('Unable to get version number from git tags')
-            exit(1)
+    if git_version.count('-') < 2:
+        return '0.0.0'
+    release, revision, commit, dirty = regex_version.match(git_version).groups()
 
-        if version.count('-') < 2:
-            return '0.0.0'
-        release, revision, commit, dirty = regex_version.match(version).groups()
-
-        version = release
-        if revision != '0':
-            version += '.post' + revision
-        if dirty:
-            version += '.dev1'
-
-    else:
-        raise NotImplementedError()
+    version = release
+    if revision != '0':
+        version += '.post' + revision
+    if dirty:
+        version += '.dev1'
 
     return version
-
 
 if __name__ == '__main__':
     print(get_version())
