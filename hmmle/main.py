3280f4a58ca1c174cbfa9c9c5bcb507d1974d68e
Lipen <lipen00@gmail.com>
2018-02-05 18:03:10 +0300

    Improve python version compatibility (tested on 2.7, 3.3, 3.5, 3.6)

--- hmmle/main.py
+++ hmmle/main.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import time
 from multiprocessing import Pool
 from itertools import permutations
@@ -13,7 +15,7 @@ from version import __version__
 
 def parse_input(filename, names, y):
     if filename:
-        log_info(f'Reading data from <{filename}>...')
+        log_info('Reading data from <{}>...'.format(filename))
         with click.open_file(filename) as f:
             lines = f.read().strip().split('\n')
             assert len(lines) >= 11, "File must contain header with names and 10 rows of data (patterns and y values)"
@@ -41,7 +43,7 @@ def parse_input(filename, names, y):
 
 def parse_models(ctx, param, value):
     if len(value) == 0 or 'all' in value:
-        return ('2H1', '2H2')
+        return '2H1', '2H2'
     else:
         assert all(map(get_model_func, value)), "Unknown model name (or anything else...)"
         return value
@@ -67,20 +69,23 @@ def parse_best(ctx, param, value):
               default=('Name1', 'Name2', 'Name3', 'Name4'),
               help='Space-separated list of ' + click.style('four', bold=True) + ' species names')
 @click.option('-y', nargs=10, metavar='<int...>', type=int,
-              help='Space-separated list of ' + click.style('ten', bold=True) + ' y values (y11 y12 y13 y14 y22 y23 y24 y33 y34 y44)')
+              help='Space-separated list of ' + click.style('ten', bold=True) +
+                   ' y values (y11 y12 y13 y14 y22 y23 y24 y33 y34 y44)')
 @click.option('-r', nargs=4, metavar='<float...>', type=float,
               default=(1, 1, 1, 1), show_default=True,
               help='Space-separated list of ' + click.style('four', bold=True) + ' r values')
 @click.option('-m', '--model', 'model_names', multiple=True, metavar='<int|all>', callback=parse_models,
-              help='Which model to use. Pass multiple times to use many models. Pass "all" to use all available models (default behaviour)')
+              help='Which model to use. Pass multiple times to use many models. '
+                   'Pass "all" to use all available models (default behaviour)')
 @click.option('--best', metavar='<int|all>', callback=parse_best,
               default='all', show_default=True,
               help='Number of best models to show')
 @click.option('--method', type=click.Choice(['SLSQP', 'L-BFGS-B', 'TNC']),
               default='SLSQP', show_default=True,
               help='Optimization method')
-@click.option('--theta0', nargs=5, type=float, metavar='<n0 T1 T3 γ1 γ3>',
-              help='Space-separated list of ' + click.style('five', bold=True) + ' initial theta components (n0 T1 T3 gamma1 gamma3)')
+@click.option('--theta0', nargs=5, type=float, metavar='<n0 T1 T3 g1 g3>',
+              help='Space-separated list of ' + click.style('five', bold=True) +
+                   ' initial theta components (n0 T1 T3 gamma1 gamma3)')
 @click.option('--only-first', is_flag=True,
               help='Do calculations only for first (initial) permutation')
 @click.option('--only-a', is_flag=True,
@@ -94,14 +99,14 @@ def parse_best(ctx, param, value):
               help='Debug')
 @click.version_option(__version__)
 def cli(filename, names, y, r, model_names, best, method, theta0, only_first, only_a, parallel, test, debug):
-    """Hybridization Networks Maximum Likelihood Estimator
+    """Hybridization Models Maximum Likelihood Estimator
 
     Author: Konstantin Chukharev (lipen00@gmail.com)
     """
 
     if debug:
         for arg, value in list(locals().items())[::-1]:
-            log_debug(f'{arg} = {value}')
+            log_debug('{} = {}'.format(arg, value))
 
     if test:
         filename = None
@@ -110,7 +115,8 @@ def cli(filename, names, y, r, model_names, best, method, theta0, only_first, on
         model_names = tuple('1P1 1P2 1T1 1T2 1PH1 1PH2 1H1 1H2 1H3 1H4 '
                             '1HP 2H1 2P1 2P2 2PH1 2PH2 2T1 2T2 2HP 2HA 2HB 2H2'.split())
         log_info('Running in test mode equivalent to the following invocation:\n'
-                 f'$ hmmle --names {names} -y {y} {" ".join(map(lambda m: f"-m {m}", model_names))}')
+                 '$ hmmle --names {} -y {} {}'
+                 .format(names, y, " ".join(map(lambda m: '-m {}'.format(m), model_names))))
         names = tuple(names.split())
         y = tuple(map(int, y.split()))
 
@@ -121,7 +127,7 @@ def cli(filename, names, y, r, model_names, best, method, theta0, only_first, on
     if len(theta0) == 0:
         theta0 = (0.6 * sum(data.values()), 0.5, 0.5, 0.5, 0.5)
         if debug:
-            log_debug(f'Default theta0: {theta0}')
+            log_debug('Default theta0: {}'.format(theta0))
 
     if only_a:
         log_info('Doing only a_ij calculations...')
@@ -133,20 +139,20 @@ def cli(filename, names, y, r, model_names, best, method, theta0, only_first, on
             print_results(a, data, model_name, theta0, r)
 
         time_total = time.time() - time_start
-        log_success(f'Done in {time_total:.1f} s.')
+        log_success('Done in {:.1f} s.'.format(time_total))
     else:
-        log_info(f'Doing calculations (method: {method})...')
+        log_info('Doing calculations (method: {})...'.format(method))
         time_start = time.time()
 
         a_hat = sum(data.values()) / 10
         L0 = 10 * a_hat * (np.log(a_hat) - 1)
         if debug:
-            log_debug(f'a_hat: {a_hat:.3f}')
-            log_debug(f'L_0: {L0:.3f}')
+            log_debug('a_hat: {:.3f}'.format(a_hat))
+            log_debug('L_0: {:.3f}'.format(L0))
 
         for model_name in model_names:
             click.echo('=' * 70)
-            log_info(f'Optimizing model {model_name}...')
+            log_info('Optimizing model {}...'.format(model_name))
             time_solve_start = time.time()
 
             theta_bounds = get_model_theta_bounds(model_name)
@@ -171,15 +177,17 @@ def cli(filename, names, y, r, model_names, best, method, theta0, only_first, on
             for permutation, result in it:
                 results[permutation] = result
                 if debug:
-                    log_debug(f'Permutation [{", ".join(fix(species, permutation))}] done after {result.nit} iterations')
+                    log_debug('Permutation [{}] done after {} iterations'
+                              .format(", ".join(fix(species, permutation)), result.nit))
                 if not result.success:
-                    log_error(f'Optimization for model {model_name} failed on permutation [{", ".join(fix(species, permutation))}] with message: {result.message}')
+                    log_error('Optimization for model {} failed on permutation [{}] with message: {}'
+                              .format(model_name, ", ".join(fix(species, permutation)), result.message))
                     if debug:
-                        log_debug(f'result:\n{result}')
+                        log_debug('result:\n{}'.format(result))
                     break
             else:
                 time_solve = time.time() - time_solve_start
-                log_success(f'Done optimizing model {model_name} in {time_solve:.1f} s.')
+                log_success('Done optimizing model {} in {:.1f} s.'.format(model_name, time_solve))
 
             if parallel > 1:
                 pool.join()
@@ -191,19 +199,18 @@ def cli(filename, names, y, r, model_names, best, method, theta0, only_first, on
                 tmp = tmp[:best]
 
             if tmp:
-                log_info(f'Hybridization model {model_name}')
+                log_info('Hybridization model {}'.format(model_name))
                 for i, (permutation, result) in enumerate(tmp, start=1):
                     fit = -result.fun
                     ratio = 2 * (fit - L0)
-                    n0, T1, T3, gamma1, gamma3 = result.x
-                    print_best(i, best, fix(species, permutation), fit, ratio, n0, T1, T3, gamma1, gamma3)
+                    print_best(i, best, fix(species, permutation), fit, ratio, result.x)
 
                     a = get_all_a(model_func, permutation, result.x, r)
-                    print_rock(a, data, model_name, permutation, result.x, r)
+                    print_rock(a, data, permutation)
 
         click.echo('=' * 70)
         time_total = time.time() - time_start
-        log_success(f'All done in {time_total:.1f} s.')
+        log_success('All done in {:.1f} s.'.format(time_total))
 
 
 if __name__ == '__main__':
--- hmmle/printers.py
+++ hmmle/printers.py
@@ -6,31 +6,31 @@ import click
 from .utils import fix, pattern2ij
 
 
-def log(text, symbol, *, fg=None, bg=None, bold=None, nl=True):
+def log(text, symbol, fg=None, bg=None, bold=None, nl=True):
     if symbol is None:
         pre = ''
     else:
-        pre = f'[{symbol: >1}] '
-    click.secho(f'{pre}{text}', fg=fg, bg=bg, bold=bold, nl=nl)
+        pre = '[{: >1}] '.format(symbol)
+    click.secho('{}{}'.format(pre, text), fg=fg, bg=bg, bold=bold, nl=nl)
 
 
-def log_debug(text, *, symbol='.', fg='white', bg=None, bold=None, nl=True):
+def log_debug(text, symbol='.', fg='white', bg=None, bold=None, nl=True):
     log(text, symbol, fg=fg, bg=bg, bold=bold, nl=nl)
 
 
-def log_info(text, *, symbol='*', fg='blue', bg=None, bold=True, nl=True):
+def log_info(text, symbol='*', fg='blue', bg=None, bold=True, nl=True):
     log(text, symbol, fg=fg, bg=bg, bold=bold, nl=nl)
 
 
-def log_success(text, *, symbol='+', fg='green', bg=None, bold=True, nl=True):
+def log_success(text, symbol='+', fg='green', bg=None, bold=True, nl=True):
     log(text, symbol, fg=fg, bg=bg, bold=bold, nl=nl)
 
 
-def log_warn(text, *, symbol='!', fg='magenta', bg=None, bold=True, nl=True):
+def log_warn(text, symbol='!', fg='magenta', bg=None, bold=True, nl=True):
     log(text, symbol, fg=fg, bg=bg, bold=bold, nl=nl)
 
 
-def log_error(text, *, symbol='!', fg='red', bg=None, bold=True, nl=True):
+def log_error(text, symbol='!', fg='red', bg=None, bold=True, nl=True):
     log(text, symbol, fg=fg, bg=bg, bold=bold, nl=nl)
 
 
@@ -39,40 +39,45 @@ def print_data(data):
     click.secho('  pattern ij  y_ij', bold=True)
     for pattern, y_ij in sorted(data.items(), key=lambda t: pattern2ij(t[0])):
         i, j = pattern2ij(pattern)
-        click.echo(f'    {pattern}  {i}{j}  {y_ij: >3}')
+        click.echo('    {}  {}{}  {: >3}'.format(pattern, i, j, y_ij))
 
 
 def print_species(species):
-    log_info(f'{len(species)} species: {", ".join(species)}')
+    log_info('{} species: {}'
+             .format(len(species), ", ".join(species)))
 
 
 def print_results(a, data, model, theta, r):
-    log_success(f'Result for hybridization model {model} with theta = {theta}, r = {r}:')
+    log_success('Result for hybridization model {} with theta = {}, r = {}:'
+                .format(model, theta, r))
     click.secho('  pattern ij  y_ij  a_ij', bold=True)
     for pattern in sorted(a, key=lambda p: pattern2ij(p)):
         a_ij = a[pattern]
         y_ij = data[pattern]
         i, j = pattern2ij(pattern)
-        click.echo(f'    {pattern}  {i}{j}  {y_ij: >3}  {a_ij: >6.3f}')
+        click.echo('    {}  {}{}  {: >3}  {: >6.3f}'
+                   .format(pattern, i, j, y_ij, a_ij))
 
 
-def print_best(i, best, species, fit, ratio, n0, T1, T3, gamma1, gamma3):
-    log_info(f'Best #{i} of {best}')
-    click.echo(click.style(' -   perm:', bold=True) + f' {", ".join(species)}')
-    click.echo(click.style(' -    fit:', bold=True) + f' {fit:.3f}')
-    click.echo(click.style(' -  ratio:', bold=True) + f' {ratio:.3f}')
-    click.echo(click.style(' -     n0:', bold=True) + f' {n0:.3f}')
-    click.echo(click.style(' -     T1:', bold=True) + f' {T1:.4f}')
-    click.echo(click.style(' -     T3:', bold=True) + f' {T3:.4f}')
-    click.echo(click.style(' - gamma1:', bold=True) + f' {gamma1:.4f}')
-    click.echo(click.style(' - gamma3:', bold=True) + f' {gamma3:.4f}')
+def print_best(i, best, species, fit, ratio, theta):
+    n0, T1, T3, gamma1, gamma3 = theta
+    log_info('Best #{} of {}'.format(i, best))
+    click.echo(click.style(' -   perm:', bold=True) + ' {}'.format(", ".join(species)))
+    click.echo(click.style(' -    fit:', bold=True) + ' {:.3f}'.format(fit))
+    click.echo(click.style(' -  ratio:', bold=True) + ' {:.3f}'.format(ratio))
+    click.echo(click.style(' -     n0:', bold=True) + ' {:.3f}'.format(n0))
+    click.echo(click.style(' -     T1:', bold=True) + ' {:.4f}'.format(T1))
+    click.echo(click.style(' -     T3:', bold=True) + ' {:.4f}'.format(T3))
+    click.echo(click.style(' - gamma1:', bold=True) + ' {:.4f}'.format(gamma1))
+    click.echo(click.style(' - gamma3:', bold=True) + ' {:.4f}'.format(gamma3))
 
 
-def print_rock(a, data, model, permutation, theta, r):
+def print_rock(a, data, permutation):
     log_info('For those about to rock:')
     click.secho('  pattern ij  y_ij  a_ij', bold=True)
     for pattern, y_ij in sorted(data.items(), key=lambda t: pattern2ij(fix(t[0], permutation))):
         pattern = fix(pattern, permutation)
         a_ij = a[pattern]
         i, j = pattern2ij(pattern)
-        click.echo(f'    {pattern}  {i}{j}  {y_ij: >3}  {a_ij: >6.3f}')
+        click.echo('    {}  {}{}  {: >3}  {: >6.3f}'
+                   .format(pattern, i, j, y_ij, a_ij))
