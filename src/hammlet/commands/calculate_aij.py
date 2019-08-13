f319149d9018ed23f5a0d60242e104652ca15742
Konstantin Chukharev <lipen00@gmail.com>
2019-08-13 23:43:34 +0300

    Remove unnecessary import

--- src/hammlet/commands/calculate_aij.py
+++ src/hammlet/commands/calculate_aij.py
@@ -6,7 +6,7 @@ from ..optimizer import Optimizer
 from ..parsers import presets_db, parse_best, parse_input, parse_models
 from ..printers import (log, log_br, log_debug, log_info, log_success, log_warn, print_a, print_input,
                         print_model_results, print_permutation, print_model_result_boot)
-from ..utils import autotimeit, pformatf, get_a, get_chains, morph4
+from ..utils import autotimeit, pformatf, get_a, morph4
 from ..version import version as __version__
 
 
