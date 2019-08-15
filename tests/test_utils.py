7d4454587bf3e11937ad172351a850a78f10c30a
Konstantin Chukharev <lipen00@gmail.com>
2019-08-15 17:01:59 +0300

    Fix get_paths arguments order

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,16 +3,16 @@ from hammlet.models import models_hierarchy
 
 
 def test_get_paths_H1_free():
-    assert all(path[-1] == 'PL1' for path in get_paths('2H1', models_hierarchy['H1']['free']))
+    assert all(path[-1] == 'PL1' for path in get_paths(models_hierarchy['H1']['free'], '2H1'))
 
 
 def test_get_paths_H1_nonfree():
-    assert all(path[-1] == 'PL1' for path in get_paths('2H1', models_hierarchy['H1']['non-free']))
+    assert all(path[-1] == 'PL1' for path in get_paths(models_hierarchy['H1']['non-free'], '2H1'))
 
 
 def test_get_paths_H2_free():
-    assert all(path[-1] == 'PL2' for path in get_paths('2H2', models_hierarchy['H2']['free']))
+    assert all(path[-1] == 'PL2' for path in get_paths(models_hierarchy['H2']['free'], '2H2'))
 
 
 def test_get_paths_H2_nonfree():
-    assert all(path[-1] == 'PL2' for path in get_paths('2H2', models_hierarchy['H2']['non-free']))
+    assert all(path[-1] == 'PL2' for path in get_paths(models_hierarchy['H2']['non-free'], '2H2'))
