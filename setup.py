2d61647ac56f42c8049b155274575ffd7eabad64
Konstantin Chukharev <lipen00@gmail.com>
2019-08-18 03:48:24 +0300

    Minimal python3 version is 3.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def main():
         author="Konstantin Chukharev",
         author_email="lipen00@gmail.com",
         license="GNU GPLv3",
-        python_requires=">=2.7, !=3.0.*",
+        python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
         package_dir={"": "src"},
         packages=find_packages("src"),
         use_scm_version={
