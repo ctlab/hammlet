17e50f5fcf13e386c5fc18a928bae7e1e0f11fdf
Konstantin Chukharev <lipen00@gmail.com>
2020-03-15 22:38:15 +0300

    Fix polytomy model name in tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,14 +4,13 @@ from hammlet.utils import get_paths
 
 def test_get_paths_H1_free():
     assert all(
-        path[-1] == "PL1" for path in get_paths(models_hierarchy["H1"]["free"], "2H1")
+        path[-1] == "P" for path in get_paths(models_hierarchy["H1"]["free"], "2H1")
     )
 
 
 def test_get_paths_H1_nonfree():
     assert all(
-        path[-1] == "PL1"
-        for path in get_paths(models_hierarchy["H1"]["non-free"], "2H1")
+        path[-1] == "P" for path in get_paths(models_hierarchy["H1"]["non-free"], "2H1")
     )
 
 
