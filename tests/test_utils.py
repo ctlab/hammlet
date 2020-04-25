7395afb75eaf9da282defdd298a91f5367d183cf
Konstantin Chukharev <lipen00@gmail.com>
2020-04-26 01:41:36 +0300

    Fix messed up tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,24 +4,23 @@ from hammlet.utils import get_paths
 
 def test_get_paths_H1_free():
     assert all(
-        path[-1] == "P" for path in get_paths(models_hierarchy["H1"]["fixed"], "2H1")
+        path[-1] == "P" for path in get_paths(models_hierarchy["H1"]["free"], "2H1")
     )
 
 
 def test_get_paths_H1_nonfree():
     assert all(
-        path[-1] == "P" for path in get_paths(models_hierarchy["H1"]["non-free"], "2H1")
+        path[-1] == "P" for path in get_paths(models_hierarchy["H1"]["fixed"], "2H1")
     )
 
 
 def test_get_paths_H2_free():
     assert all(
-        path[-1] == "PL2" for path in get_paths(models_hierarchy["H2"]["fixed"], "2H2")
+        path[-1] == "PL2" for path in get_paths(models_hierarchy["H2"]["free"], "2H2")
     )
 
 
 def test_get_paths_H2_nonfree():
     assert all(
-        path[-1] == "PL2"
-        for path in get_paths(models_hierarchy["H2"]["non-free"], "2H2")
+        path[-1] == "PL2" for path in get_paths(models_hierarchy["H2"]["fixed"], "2H2")
     )
