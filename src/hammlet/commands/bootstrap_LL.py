f29d4d4bcd527f17150dfa6c618dbf2cc6ab970b
Konstantin Chukharev <lipen00@gmail.com>
2021-12-12 20:03:42 +0300

    Change bootstrap-ll

--- src/hammlet/commands/bootstrap_LL.py
+++ src/hammlet/commands/bootstrap_LL.py
@@ -37,22 +37,31 @@ from ..utils import autotimeit, pformatf
     help="Space-separated list of " + click.style("four", bold=True) + " r values",
 )
 @click.option(
-    "-m",
-    "--models",
-    "models",
-    nargs=2,
-    metavar="<name1 name2>",
+    "-l",
+    "--level",
+    "level_senior",
+    metavar="<level>",
     required=True,
-    callback=parse_models,
-    help="Two models for bootstrap",
+    type=click.Choice(["N{}".format(i) for i in range(0, 4 + 1)]),
+    help="Level (senior) for bootstrap",
 )
 @click.option(
-    "--permutation",
-    metavar="<int...>",
+    "-m",
+    "--model",
+    "models",
+    metavar="<name>",
+    multiple=True,  # actually, this arg must be single, but the parser only supports list of models
     required=True,
-    callback=parse_permutation,
-    help="Permutations to use for calculations",
+    callback=parse_models,
+    help="Model (junior) for bootstrap",
 )
+# @click.option(
+#     "--permutation",
+#     metavar="<int...>",
+#     required=True,
+#     callback=parse_permutation,
+#     help="Permutations to use for calculations",
+# )
 @click.option(
     "-n",
     "--times",
@@ -91,8 +100,9 @@ def bootstrap_LL(
     preset,
     y,
     r,
+    level_senior,
     models,
-    permutation,
+    # permutation,
     bootstrap_times,
     output_filename_bootstrap,
     method,
@@ -101,34 +111,41 @@ def bootstrap_LL(
 ):
     """Perform MLE bootstrap-LL."""
 
-    if len(models) != 2:
-        raise click.BadParameter("specify exactly two models", param_hint="-m/--models")
+    if len(models) != 1:
+        raise click.BadParameter(
+            "specify exactly one (junior) model", param_hint="-m/--models"
+        )
 
     y = parse_input(preset, y, verbose=True)
     del preset
 
-    # Determine senior/junior models
-    for i in range(0, 4 + 1):
-        ms = models_nrds["N{}".format(i)]
-        if models[0] in ms:
-            n1, m1 = i, models[0]
-        if models[1] in ms:
-            n2, m2 = i, models[1]
-    if n1 == n2:
-        raise click.BadParameter(
-            "specify two models from different N-levels", param_hint="-m/--models"
-        )
-    if n1 > n2:
-        model_senior, model_junior = m1, m2
-    else:
-        model_senior, model_junior = m2, m1
+    model_junior = models[0]
     del models
+
+    models_senior = models_nrds[level_senior]
+
+    # # Determine senior/junior models
+    # for i in range(0, 4 + 1):
+    #     ms = models_nrds["N{}".format(i)]
+    #     if models[0] in ms:
+    #         n1, m1 = i, models[0]
+    #     if models[1] in ms:
+    #         n2, m2 = i, models[1]
+    # if n1 == n2:
+    #     raise click.BadParameter(
+    #         "specify two models from different N-levels", param_hint="-m/--models"
+    #     )
+    # if n1 > n2:
+    #     model_senior, model_junior = m1, m2
+    # else:
+    #     model_senior, model_junior = m2, m1
+    # del models
 
     log_info("y: {}".format(" ".join(map(str, y))))
     log_info("r: ({})".format(", ".join(map(pformatf, r))))
-    log_info("Senior model: {}".format(model_senior.name))
+    log_info("Senior level: {}".format(level_senior))
     log_info("Junior model: {}".format(model_junior.name))
-    log_info("Permutation: ({})".format(",".join(map(str, permutation))))
+    # log_info("Permutation: ({})".format(",".join(map(str, permutation))))
     log_info("Output file: {}".format(output_filename_bootstrap))
 
     if not theta0:
@@ -137,30 +154,37 @@ def bootstrap_LL(
             log_debug("Using default theta0: {}".format(theta0))
 
     log_info(
-        "Bootstraping models {}/{} (senior/junior) {} times...".format(
-            model_senior.name, model_junior.name, bootstrap_times
+        "Bootstraping {}/{} (senior/junior) {} times...".format(
+            level_senior, model_junior.name, bootstrap_times
         )
     )
     data = []
     for _ in range(bootstrap_times):
         y_poissoned = tuple(poisson(y))
         optimizer_boot = Optimizer(y_poissoned, r, theta0, method, debug=debug)
-        result_boot_senior = optimizer_boot.one(model_senior, permutation)
-        result_boot_junior = optimizer_boot.one(model_junior, permutation)
-        LLx = result_boot_senior.LL
-        LLy = result_boot_junior.LL
+
+        results_boot_senior = optimizer_boot.many(models_senior)
+        results_boot_junior = optimizer_boot.many_perms(model_junior, "all")
+
+        best_result_senior = max(results_boot_senior, key=lambda it: it.LL)
+        best_result_junior = max(results_boot_junior, key=lambda it: it.LL)
+
+        LLx = best_result_senior.LL
+        LLy = best_result_junior.LL
         LL_diff = LLx - LLy
         data.append(
             (
                 " ".join(format(x, " >2") for x in y_poissoned),
-                model_senior.name,
+                best_result_senior.model.name,
+                "".join(map(str, best_result_senior.permutation)),
                 LLx,
                 model_junior.name,
+                "".join(map(str, best_result_junior.permutation)),
                 LLy,
                 LL_diff,
             )
         )
-    headers = ["y", "Mx", "LLx", "My", "LLy", "LLx-LLy"]
+    headers = ["y", "Mx", "permx", "LLx", "My", "permy", "LLy", "LLx-LLy"]
     if output_filename_bootstrap:
         log_info(
             "Writing bootstrap results to <{}>...".format(output_filename_bootstrap)
